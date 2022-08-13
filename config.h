18279bd1a332e1af513158a107e1480de8f6a463
Jericho Keyne <jerichokeyne@gmail.com>
2022-08-13 17:32:39 -0600

    Updated keymap, and config file to hopefully work now

--- config.h
+++ config.h
@@ -1,17 +1,23 @@
-/* USB Device descriptor parameter */
-#define PRODUCT_ID   0x3435
-#define DEVICE_VER   0x0001
-#define MANUFACTURER tshort
-
-/* key matrix size */
-// Rows are doubled-up
-#define MATRIX_ROWS 10
-#define MATRIX_COLS 5
+// /* USB Device descriptor parameter */
+// #define PRODUCT_ID   0x3435
+// #define DEVICE_VER   0x0001
+// #define MANUFACTURER tshort
+//
+// /* key matrix size */
+// // Rows are doubled-up
+// #define MATRIX_ROWS 10
+// #define MATRIX_COLS 5
 
 // wiring of each half
 // Arduino Pin Labels:    A4, A3, A2, A1, A0
+#ifdef MATRIX_ROW_PINS
+#undef MATRIX_ROW_PINS
+#endif
 #define MATRIX_ROW_PINS { F1, F4, F5, F6, F7 }
 // Arduino Pin Labels:    D4, D5, D2, D7, D9
+#ifdef MATRIX_COL_PINS
+#undef MATRIX_COL_PINS
+#endif
 #define MATRIX_COL_PINS { D4, C6, D1, E6, B5 }
 
 #define DIODE_DIRECTION COL2ROW
