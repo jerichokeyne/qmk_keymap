482bf85809466f03601b983ece340627afc85f94
Jericho Keyne <jericho.keyne@microfocus.com>
2022-08-12 23:06:16 -0600

    Initial commit

---
+++ config.h
@@ -0,0 +1,4 @@
+// Arduino Pin Labels:    A4, A3, A2, A1, A0
+#define MATRIX_ROW_PINS { F1, F4, F5, F6, F7 }
+// Arduino Pin Labels:    D4, D5, D2, D7, D9
+#define MATRIX_COL_PINS { D4, C6, D1, E6, B5 }
