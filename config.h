ee2d8345720b54b6457bd541417bc253e029a6ba
Jericho Keyne <jerichokeyne@gmail.com>
2022-08-13 19:32:45 -0600

    Added some ifdefs

--- config.h
+++ config.h
@@ -21,9 +21,15 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
+#ifndef PRODUCT_ID
 #define PRODUCT_ID   0x3435
+#endif
+#ifndef DEVICE_VER
 #define DEVICE_VER   0x0001
+#endif
+#ifndef MANUFACTURER
 #define MANUFACTURER jkeyne
+#endif
 
 /* key matrix size */
 // Rows are doubled-up
