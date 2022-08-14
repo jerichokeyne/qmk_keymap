22a5d6e2eb2cde0616b7854748643fb90ac68ddd
Jericho Keyne <jerichokeyne@gmail.com>
2022-08-13 19:27:39 -0600

    Switched to just having the keyboard config

--- config.h
+++ config.h
@@ -1,32 +1,40 @@
-// /* USB Device descriptor parameter */
-// #define PRODUCT_ID   0x3435
-// #define DEVICE_VER   0x0001
-// #define MANUFACTURER tshort
-//
-// /* key matrix size */
-// // Rows are doubled-up
-// #define MATRIX_ROWS 10
-// #define MATRIX_COLS 5
+/*
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define PRODUCT_ID   0x3435
+#define DEVICE_VER   0x0001
+#define MANUFACTURER jkeyne
+
+/* key matrix size */
+// Rows are doubled-up
+#define MATRIX_ROWS 10
+#define MATRIX_COLS 5
 
 // wiring of each half
 // Arduino Pin Labels:    A4, A3, A2, A1, A0
-#ifdef MATRIX_ROW_PINS
-#undef MATRIX_ROW_PINS
-#endif
 #define MATRIX_ROW_PINS { F1, F4, F5, F6, F7 }
+// #define MATRIX_COL_PINS { B5, B4, E6, D7, C6 }
 // Arduino Pin Labels:    D4, D5, D2, D7, D9
-#ifdef MATRIX_COL_PINS
-#undef MATRIX_COL_PINS
-#endif
 #define MATRIX_COL_PINS { D4, C6, D1, E6, B5 }
 
 #define DIODE_DIRECTION COL2ROW
-
-#define USE_SERIAL
-//#define USE_I2C
-
-/* Select hand configuration */
-#define MASTER_LEFT
-//#define MASTER_RIGHT
-
-#define EE_HANDS
---
+++ keymaps/default/config.h
@@ -0,0 +1,8 @@
+#define USE_SERIAL
+//#define USE_I2C
+
+/* Select hand configuration */
+#define MASTER_LEFT
+//#define MASTER_RIGHT
+
+#define EE_HANDS
